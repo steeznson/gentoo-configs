f69646eab778925080ea5b52e5a91e834c993c35
James Stevenson <james.al.stevenson@gmail.com>
2025-12-24 12:03:33 +0000

    new emacs config

---
+++ dotfiles/.emacs.d/custom.el
@@ -0,0 +1,174 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-battery-mode t)
+ '(display-time-mode t)
+ '(elfeed-feeds
+   '("www.theweek.co.uk/feeds/all" "old.reddit.com/r/hiphopheads.rss" "old.reddit.com/r/tennis.rss" "old.reddit.com/r/rugbyunion.rss" "old.reddit.com/r/games.rss"))
+ '(package-selected-packages
+   '(radio slime nov hackernews gnu-elpa-keyring-update magit ac-c-headers elfeed paredit markdown-mode))
+ '(send-mail-function 'smtpmail-send-it)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Roboto Mono" :foundry "pyrs" :slant normal :weight regular :height 82 :width normal)))))
+
+;; Mode Line Config
+(setq display-time-default-load-average nil)
+(display-time-mode t)
+(display-battery-mode 1)
+(setq-default mode-line-format
+	      '("Λ:%m"
+		"  Π:%b"
+		"  ω:%l"
+		"  τ:"
+		display-time-string
+		"  Δ:"
+		battery-mode-line-string))
+
+;; Eww Browser
+(setq eww-download-directory "~/downloads/")
+(setq browse-url-browser-function 'eww-browse-url)
+
+;; ERC
+(setq
+ erc-nick "steeznson"
+ erc-user-full-name "James Stevenson"
+ erc-server "irc.libera.chat"
+ erc-port "6667")
+
+;; Mastodon
+(setq mastodon-instance-url "https://glasgow.social"
+      mastodon-active-user "steeznson@glasgow.social")
+
+;; Global Formatting Configuration
+(setq-default indent-tabs-mode nil)
+(setq-default fill-column 80)
+(setq show-paren-delay 0)
+(show-paren-mode)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq c-basic-offset 4)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+
+;; Disable backup files
+(setq make-backup-files nil)
+
+;; Disable lockfiles.
+(setq create-lockfiles nil)
+
+;; dotnet
+(use-package csharp-mode
+  :if (executable-find "dotnet")
+  :mode
+  (("\\.cake\\'" . csharp-mode)
+   ("\\.cs\\'"   . csharp-mode))
+  :config
+  (progn
+    (c-toggle-auto-hungry-state 1)
+    (c-toggle-auto-newline 1))
+  (add-hook 'csharp-mode-hook 'toggle-truncate-lines)
+  (add-hook 'csharp-mode-hook 'display-line-numbers-mode)
+  )
+(use-package fsharp-mode
+  :if (executable-find "dotnet")
+  :mode
+  (("\\.fs\\'"  . fsharp-mode)
+   ("\\.fsx\\'" . fsharp-mode)))
+
+;; Change default location of Mail and News
+(setq message-directory "~/.emacs.d/mail/")
+(setq gnus-directory "~/.emacs.d/news/")
+(setq nnfolder-directory "~/.emacs.d/mail/archive")
+(setq nnfolder-active-file "~/.emacs.d/mail/archive/active")
+
+;; Radio stations
+(customize-set-variable
+ 'radio-command
+ '("mpv" "--terminal=no" "--video=no" :url))
+
+(customize-set-variable
+ 'radio-stations-alist
+ '(("BBC World" . "http://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/pc_hd_abr_v2/ak/bbc_world_service.m3u8")
+   ("Times Radio" . "http://www.radiofeeds.net/playlists/timesb.pls?mp=stream-mobile")
+   ))
+
+;; Custom Functions
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+	    (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+(defun volume-up()
+  (interactive)
+  (shell-command "pactl set-sink-volume 1 +5%"))
+(defun volume-down()
+  (interactive)
+  (shell-command "pactl set-sink-volume 1 -5%"))
+(defun bright-up()
+  (interactive)
+  (shell-command "xbacklight -inc 10"))
+(defun bright-down()
+  (interactive)
+  (shell-command "xbacklight -dec 10"))
+(defun remove-all-specified-elements (element-tag)
+  (save-excursion
+    (let ((case-fold-search nil))
+      (while (search-forward-regexp (concat "<" element-tag "[^\\>]*>"))
+	(delete-region
+	 (match-beginning 0)
+	 (search-forward (concat "</" element-tag ">")))))))
+(defun alist-keys (alist)
+  (mapcar 'car alist))
+(defun get-json (uri)
+  "Fetch the contents of URI and parse."
+  (with-current-buffer (url-retrieve-synchronously uri)
+    (goto-char (point-min))
+    (goto-char url-http-end-of-headers)
+    (prog1 (json-read)
+      (kill-buffer))))
+(defun catreplies (replies)
+  (mapconcat (lambda (x)
+            (format "\n%s\n"
+                    (cdr (assoc-string "com" x))))
+             replies ""))
+(defun random-gee ()
+  (interactive)
+  (setq catalog
+    (get-json "https://a.4cdn.org/g/catalog.json"))
+  (setq thread (aref (cdr (assoc-string "threads" (aref catalog 0))) (random 10)))
+  (setq op
+      (format "\n%s\t%s\t%s\n%s"
+              (cdr (assoc-string "no" thread))
+              (cdr (assoc-string "name" thread))
+              (cdr (assoc-string "sub" thread))
+              (cdr (assoc-string "com" thread))))
+  (setq rawreplies (cdr (assoc-string "last_replies" thread)))
+  (setq replies (catreplies rawreplies))
+  (setq content (concat op replies))
+  (with-current-buffer (get-buffer-create "*/g/*") (insert content))
+  (switch-to-buffer "*/g/*"))
+
+;; Shortcuts
+(global-set-key (kbd "C-x /") 'display-line-numbers-mode) ;;toggle lines
+(global-set-key (kbd "C-x w") 'toggle-truncate-lines) ;;toggle line wra
+(global-set-key (kbd "C-x p") 'send-invisible) ;;put password in minibuffer
+(global-set-key (kbd "C-x t") 'untabify) ;; untabify shortcut
+(global-set-key (kbd "C-x a") 'comment-or-uncomment-region-or-line) ;;comment region
+(global-set-key (kbd "C-x ,") 'dumb-jump-go) ;;jump to def
+(global-set-key
+ (kbd "C-x n C-x t")
+ (lambda()
+   (interactive)
+   (remove-all-specified-elements
+    (read-string "Enter the element you wish to destroy: ")))) ;;destroy all undesired elements
--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,147 +1,71 @@
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(auth-source-save-behavior nil)
- '(column-number-mode t)
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   '("6bc387a588201caf31151205e4e468f382ecc0b888bac98b2b525006f7cb3307" default))
- '(display-time-mode t)
- '(ecb-options-version "2.50")
- '(elfeed-feeds
-   '("https://planet.gentoo.org/rss20.xml" "phoronix.com/rss.php" "https://feeds.bbci.co.uk/sport/tennis/rss.xml" "https://feeds.bbci.co.uk/sport/rugby-union/rss.xml" "https://draculadaily.substack.com/feed" "old.reddit.com/r/ukpolitics.rss" "old.reddit.com/r/games.rss"))
- '(fci-rule-color "#383838")
- '(global-emojify-mode nil)
- '(gnus-select-method '(nnhackernews ""))
- '(load-home-init-file t t)
- '(menu-bar-mode nil)
- '(package-selected-packages
-   '(auto-header auto-dim-other-buffers auto-correct aumix-mode auctex 0xc async ascii-art-to-unicode arduino-mode magit 0x0 arbitools apropospriate-theme apache-mode anzu anti-zenburn-theme 0blayout annotate ample-theme ahg ampc altcaps all alect-themes aircon-theme ahungry-theme aggressive-indent aggressive-completion afternoon-theme advice-patch adoc-mode adjust-parens adaptive-wrap ada-ref-man ada-mode ack ace-window gnu-elpa-keyring-update ivy consult mastodon paredit elfeed hackernews dumb-jump nhexl-mode ob-browser emms evil wiki-summary dockerfile-mode company-plsense flycheck-irony undo-tree irony company-jedi flycheck-pycheckers better-shell term+ djvu pdf-tools indent-tools xml+ company-c-headers yaml-mode nov sicp flycheck sudoku dired-ranger better-defaults))
- '(send-mail-function 'smtpmail-send-it)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "PfEd" :slant normal :weight normal :height 255 :width normal)))))
-
-;; Packages repo
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-;; Buffer Management
+;; Customize user interface.
+(when (display-graphic-p)
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0))
 (setq inhibit-startup-screen t)
-(kill-buffer "*Messages*")
-(setq-default message-log-max nil)
-(add-hook 'minibuffer-exit-hook
-	  '(lambda ()
-	     (let ((buffer "*Completions*"))
-	       (and (get-buffer buffer)
-		    (kill-buffer buffer)))))
-(setq inhibit-startup-buffer-menu t)
-(add-hook 'window-setup-hook 'delete-other-windows)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq initial-scratch-message "")
-(setq echo-keystrokes 0.1
-      use-dialog-box nil)
 (column-number-mode)
 
-;; Open shell same buffer
-(add-to-list 'display-buffer-alist
-             `(,(rx bos "*shell*")
-               display-buffer-same-window
-               (reusable-frames . visible)))
-
-;; Change default location of Mail and News
-(setq message-directory "~/.emacs.d/mail/")
-(setq gnus-directory "~/.emacs.d/news/")
-(setq nnfolder-directory "~/.emacs.d/mail/archive")
-(setq nnfolder-active-file "~/.emacs.d/mail/archive/active")
-
-;; Enable GNUPG
-(setf epa-pinentry-mode 'loopback)
-
-;; Disable backup files
-(setq make-backup-files nil)
-
-;; Disable lockfiles.
-(setq create-lockfiles nil)
-
-;; Global Modes
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-
 ;; Interactively do things.
-;; (ido-mode 1)
-;; (ido-everywhere)
-;; (setq ido-enable-flex-matching t)
-;; (fido-mode)
-(ivy-mode 1)
+(ido-mode 1)
+(ido-everywhere)
+(setq ido-enable-flex-matching t)
+(fido-mode)
 
-;; Make CTRL-x CTRL-u the "undo" command; this is better than "CTRL-x u"
-;; because you don't have to release the CTRL key.
-(define-key global-map "\C-x\C-u" 'undo)
+;; Show stray whitespace.
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
 
-;; Eww Browser
-(setq eww-download-directory "~/downloads/")
-(setq browse-url-browser-function 'eww-browse-url)
+;; Add a newline automatically at the end of a file while saving.
+(setq-default require-final-newline t)
 
-;; ERC
-(setq
- erc-nick "steeznson"
- erc-user-full-name "James Stevenson"
- erc-server "irc.libera.chat"
- erc-port "6667")
+;; Consider a period followed by a single space to be end of sentence.
+(setq sentence-end-double-space nil)
 
-;; Mastodon
-(setq mastodon-instance-url "https://glasgow.social"
-      mastodon-active-user "steeznson@glasgow.social")
-
-;; Global Formatting Configuration
+;; Use spaces, not tabs, for indentation.
 (setq-default indent-tabs-mode nil)
-(setq-default fill-column 80)
-(setq show-paren-delay 0)
-(show-paren-mode)
-(setq-default indent-tabs-mode nil)
+
+;; Display the distance between two tab stops as 4 characters wide.
 (setq-default tab-width 4)
+
+;; Indentation setting for various languages.
 (setq c-basic-offset 4)
 (setq js-indent-level 2)
 (setq css-indent-offset 2)
 
-;; C/C++ Development Config
-(semantic-mode 1)
-(global-ede-mode 1)
-(setq c-default-style "k&r")
-(add-hook 'c-mode-common-hook 'company-mode)
-(add-hook 'c-mode-common-hook 'irony-mode)
-(add-hook 'c-mode-common-hook 'toggle-truncate-lines)
-(add-hook 'c-mode-common-hook 'display-line-numbers-mode)
-(add-hook 'c-mode-common-hook 'flycheck-mode)
-(add-hook 'c-mode-common-hook
-	  (lambda () (local-set-key (kbd "<C-tab>") 'company-complete)))
-(add-hook 'c-mode-common-hook
-	  (lambda () (local-set-key (kbd "C-c j") 'find-tag)))
+;; Highlight matching pairs of parentheses.
+(setq show-paren-delay 0)
+(show-paren-mode)
 
-;; Python Development Configs
-(setenv "PYTHONPATH" (shell-command-to-string "$SHELL --login -c 'echo -n $PYTHONPATH'"))
-(setq py-python-command "python3")
-(setq flycheck-python-pycompile-executable "python3")
-(defcustom python-shell-interpreter "python3"
-  "Default Python interpreter for shell."
-  :type 'string
-  :group 'python)
-(add-hook 'python-mode-hook 'company-mode)
-(add-hook 'python-mode-hook 'flycheck-mode)
-(add-hook 'python-mode-hook 'toggle-truncate-lines)
-(add-hook 'python-mode-hook 'display-line-numbers-mode)
+;; Write auto-saves and backups to separate directory.
+(make-directory "~/.tmp/emacs/auto-save/" t)
+(setq auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
+(setq backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
+
+;; Do not move the current file while creating backup.
+(setq backup-by-copying t)
+
+;; Disable lockfiles.
+(setq create-lockfiles nil)
+
+;; Workaround for https://debbugs.gnu.org/34341 in GNU Emacs <= 26.3.
+(when (and (version< emacs-version "26.3") (>= libgnutls-version 30603))
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+;; Write customizations to a separate file instead of this file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
+
+;; Enable installation of packages from MELPA.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install packages.
+(dolist (package '(markdown-mode paredit))
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; Enable Paredit.
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
@@ -150,121 +74,9 @@
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 
-;; elisp development configs
-(add-hook 'emacs-lisp-mode-hook 'toggle-truncate-lines)
-(add-hook 'emacs-lisp-mode-hook 'display-line-numbers-mode)
-
-;; common lisp development configs
-(setq inferior-lisp-program (executable-find "clisp"))
-(add-hook 'lisp-mode-hook 'toggle-truncate-lines)
-(add-hook 'lisp-mode-hook 'display-line-numbers-mode)
-
-;; rust development configs
-(add-hook 'rust-mode-hook 'toggle-truncate-lines)
-(add-hook 'rust-mode-hook 'display-line-numbers-mode)
-
-;; Perl development configs
-(add-hook 'perl-mode-hook 'company-mode)
-(add-hook 'perl-mode-hook 'toggle-truncate-lines)
-(add-hook 'perl-mode-hook 'display-line-numbers-mode)
-
-;; Mode Line Config
-(setq display-time-default-load-average nil)
-(display-time-mode t)
-(display-battery-mode 1)
-(setq-default mode-line-format
-	      '("Λ:%m"
-		"  Π:%b"
-		"  ω:%l"
-		"  τ:"
-		display-time-string
-		"  Δ:"
-		battery-mode-line-string))
-
-;; emms
-(add-to-list 'load-path "~/.emacs.d/emms/lisp/")
-(require 'emms-setup)
-(emms-all)
-(emms-default-players)
-
-;; Custom Functions
-(defun comment-or-uncomment-region-or-line ()
-  "Comments or uncomments the region or the current line if there's no active region."
-  (interactive)
-  (let (beg end)
-    (if (region-active-p)
-	(setq beg (region-beginning) end (region-end))
-      (setq beg (line-beginning-position) end (line-end-position)))
-    (comment-or-uncomment-region beg end)))
-(defun volume-up()
-  (interactive)
-  (shell-command "pactl set-sink-volume 1 +5%"))
-(defun volume-down()
-  (interactive)
-  (shell-command "pactl set-sink-volume 1 -5%"))
-(defun bright-up()
-  (interactive)
-  (shell-command "xbacklight -inc 10"))
-(defun bright-down()
-  (interactive)
-  (shell-command "xbacklight -dec 10"))
-(defun remove-all-specified-elements (element-tag)
-  (save-excursion
-    (let ((case-fold-search nil))
-      (while (search-forward-regexp (concat "<" element-tag "[^\\>]*>"))
-	(delete-region
-	 (match-beginning 0)
-	 (search-forward (concat "</" element-tag ">")))))))
-(defun alist-keys (alist)
-  (mapcar 'car alist))
-(defun get-json (uri)
-  "Fetch the contents of URI and parse."
-  (with-current-buffer (url-retrieve-synchronously uri)
-    (goto-char (point-min))
-    (goto-char url-http-end-of-headers)
-    (prog1 (json-read)
-      (kill-buffer))))
-(defun catreplies (replies)
-  (mapconcat (lambda (x)
-            (format "\n%s\n" (cdr (assoc-string "com" x)))) replies ""))
-(defun random-gee ()
-  (interactive)
-  (setq catalog
-    (get-json "https://a.4cdn.org/g/catalog.json"))
-  (setq thread (aref (cdr (assoc-string "threads" (aref catalog 0))) (random 10)))
-  (setq op
-      (format "\n%s\t%s\t%s\n%s"
-              (cdr (assoc-string "no" thread))
-              (cdr (assoc-string "name" thread))
-              (cdr (assoc-string "sub" thread))
-              (cdr (assoc-string "com" thread))))
-  (setq rawreplies (cdr (assoc-string "last_replies" thread)))
-  (setq replies (catreplies rawreplies))
-  (setq content (concat op replies))
-  (with-current-buffer (get-buffer-create "*/g/*") (insert content))
-  (switch-to-buffer "*/g/*"))
-(defun i2ptoggle ()
-  (interactive)
-  (if url-proxy-services
-      (setq url-proxy-services nil)
-    (setq url-proxy-services
-      '(("http" . "127.0.0.1:4444")))))
-
-;; Shortcuts
-(global-set-key (kbd "C-x /") 'display-line-numbers-mode) ;;toggle lines
-(global-set-key (kbd "C-x w") 'toggle-truncate-lines) ;;toggle line wra
-(global-set-key (kbd "C-x p") 'send-invisible) ;;put password in minibuffer
-(global-set-key (kbd "C-x t") 'untabify) ;; untabify shortcut
-(global-set-key (kbd "C-x a") 'comment-or-uncomment-region-or-line) ;;comment region
-(global-set-key (kbd "C-x ,") 'dumb-jump-go) ;;jump to def
-(global-set-key
- (kbd "C-x n C-x t")
- (lambda()
-   (interactive)
-   (remove-all-specified-elements
-    (read-string "Enter the element you wish to destroy: ")))) ;;destroy all undesired elements
 
 ;; Start server.
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
